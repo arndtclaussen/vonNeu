5f3df53ebb98ebd9d2db92188e236cb19b9c6e45
Arndt Claussen, Linux, Lat7320 <arndt.claussen@bahlsen.com>
2025-01-10 17:03:50 +0100

    Simple game loop by reddis

--- tasks/gamestate.py
+++ tasks/gamestate.py
@@ -4,6 +4,13 @@ from sqlalchemy.orm import sessionmaker
 from config import Config  # Import your config
 
 
+from rq import Queue
+from redis import Redis
+from datetime import timedelta
+
+redis_conn = Redis(host="127.0.0.1", port="6379", decode_responses=True)
+q = Queue(connection=redis_conn) # Declare it outside
+
 
 def hello_world_rq(my_variable):
     print(f"Hello {my_variable} from the worker!")
@@ -17,7 +24,7 @@ def hello_world_rq(my_variable):
         inspector = inspect(engine) # Create an inspector
         tables = inspector.get_table_names() # Get table names
         print(f"Existing tables: {tables}")
-
+        q.enqueue_in(timedelta(seconds=10), hello_world_rq, my_variable)
 
     except Exception as e:
         session.rollback()
